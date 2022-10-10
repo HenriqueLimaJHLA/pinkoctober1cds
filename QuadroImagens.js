c560f052a4bd6122a064dddbfa8f5b1f56afa5be
Ro005 <84298912+Roberto-Dantas@users.noreply.github.com>
2022-10-09 21:33:06 -0300

    Update QuadroImagens.js

--- QuadroImagens.js
+++ QuadroImagens.js
@@ -28,8 +28,8 @@ function voltar() {
 }
 function imagens() {
     
-    img[0]= "https:encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz24VerRA6UEfm1Kt4kL4l4wZER4oqycewtg&usqp=CAU"
-    img[1]= "https:encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTilKSldDNGzHHbx2WrV2hLwv4h8o7-4i_5ig&usqp=CAU"
-    img[2]= "https:encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWwLq9rbGJQe-6_GAIMbgS2UNeJhy1DIs5Xg&usqp=CAU"   
-    img[3]= "https:encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfRJ3T8o0RybTqKV1w1kxB3V5P2k-1IsbEhA&usqp=CAU"
+    img[0]= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz24VerRA6UEfm1Kt4kL4l4wZER4oqycewtg&usqp=CAU"
+    img[1]= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTilKSldDNGzHHbx2WrV2hLwv4h8o7-4i_5ig&usqp=CAU"
+    img[2]= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWwLq9rbGJQe-6_GAIMbgS2UNeJhy1DIs5Xg&usqp=CAU"   
+    img[3]= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfRJ3T8o0RybTqKV1w1kxB3V5P2k-1IsbEhA&usqp=CAU"
 }
