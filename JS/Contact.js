9714ed96486ef1facbf31b439f0f683c6e201160
José Henrique <113061751+HenriqueLimaJHLA@users.noreply.github.com>
2022-10-12 00:53:58 -0300

    Update Contact.js

--- JS/Contact.js
+++ JS/Contact.js
@@ -17,3 +17,32 @@ menu.css({"right": "-" + menu_width + "px"});
 porCima.css({"opacity": "0", "width": "0"});
 });
 });
+
+    var valor1 = document.getElementById('div1')
+    var valor2 = document.getElementById('div2')
+    var oi
+
+function alterar() {
+    
+    if(oi === 1) {
+        oi1()
+    }else{
+        oi2()
+     
+}}
+function oi1() {
+        console.log("Teste1");
+        div1.style.cssText= 'display: none';(a)
+        div2.style.cssText= 'display: block';
+        
+        oi = 0
+      
+}  
+function oi2() {
+        console.log("Teste2");
+        div1.style.cssText= 'display: block';
+        div2.style.cssText= 'display: none';
+        
+        oi= 1
+    }     
+
