b7347f06a74cd9b72c6a85ebfca7369edaec47d0
Ro005 <84298912+Roberto-Dantas@users.noreply.github.com>
2022-10-14 12:21:51 -0300

    Update Contact.js

--- JS/Contact.js
+++ JS/Contact.js
@@ -1,21 +1,21 @@
 $(function() {
-var menu_width = 290;
-var menu = $(".menuLado");
-var menu_aberto = $(".menu-aberto");
-var menu_fechado = $(".menu-fechado");
-var porCima = $(".porCima"); 
+     var menu_width = 290
+     var menu = $(".menuLado")
+     var menu_aberto = $(".menu-aberto")
+     var menu_fechado = $(".menu-fechado")
+     var porCima = $(".porCima")
 
-menu_aberto.click(function (e) {
-e.preventDefault();
-menu.css({"right": "0px"});
-porCima.css({"opacity": "1", "width": "100%"});
-});
+     menu_aberto.click(function (e) {
+          e.preventDefault();
+          menu.css({"right": "0px"});
+          porCima.css({"opacity": "1", "width": "100%"});
+     });
 
-menu_fechado.click(function (e) {
-e.preventDefault();
-menu.css({"right": "-" + menu_width + "px"});
-porCima.css({"opacity": "0", "width": "0"});
-});
+     menu_fechado.click(function (e) {
+         e.preventDefault();
+         menu.css({"right": "-" + menu_width + "px"});
+         porCima.css({"opacity": "0", "width": "0"});
+     });
 });
 
     var valor1 = document.getElementById('falaMedico')
@@ -27,34 +27,32 @@ porCima.css({"opacity": "0", "width": "0"});
 function alterar() {
     
     if(y === 1) {
-        x1()
+        function x1(){
+            falaMedico.style.cssText= 'display: none';
+            oMapa.style.cssText= 'display: none';
+            button.style.cssText= 'display: none';
+            y = 2      
+        }
     }
     if(y === 2){
-        x3()
-    }else{
-        x2()
-     
-}}
-function x1() {
-        falaMedico.style.cssText= 'display: none';
-        oMapa.style.cssText= 'display: none';
-        button.style.cssText= 'display: none';
-        y = 2
-      
-}  
-function x2() {
-        falaMedico.style.cssText= 'display: none';
-        oMapa.style.cssText= 'display: block';
-        nomeBotao.innerHTML= 'Voltar'
-        y= 1
-}     
-function x3() {
-        falaMedico.style.cssText= 'display: none';
-        oMapa.style.cssText= 'display: none';
+        function x3(){
+            falaMedico.style.cssText= 'display: none';
+            oMapa.style.cssText= 'display: none';
         
-        setTimeout(function() {falaMedico.style.cssText= 'display: block';
-            nomeBotao.innerHTML= 'Buscar';
-            button.style.cssText= 'display: block';}, 1000)
-        y = 0
-      
-} 
+            setTimeout(function(){falaMedico.style.cssText= 'display: block'; nomeBotao.innerHTML= 'Buscar'; button.style.cssText= 'display: block';}, 1000)
+            y = 0
+        } 
+    }else{
+        function x2(){
+            falaMedico.style.cssText= 'display: none'; 
+            oMapa.style.cssText= 'display: block'; 
+            nomeBotao.innerHTML= 'Voltar'
+            y= 1
+        }          
+}}
+function giraBotao() {
+     var menu = document.getElementsByClassName('menuImg')
+     
+     menuImg.style.cssText= 'animation: rotate 5s infinite';
+
+}
